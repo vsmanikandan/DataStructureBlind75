19659a0e366d70793a9d4dafad58e128b66222a4
manvs_nbt <mani@nxtbittechnologies.com>
2026-06-21 19:51:48 +0530

    bline 75 initial commit

--- src/problems/sectionslidingwindow/LongestSubString.java
+++ src/problems/sectionslidingwindow/LongestSubString.java
@@ -2,12 +2,12 @@ package problems.sectionslidingwindow;
 
 public class LongestSubString {
 
-    public int calculateLongSubstring(String s, int freq) {
-        int count = {};
+    public void calculateLongSubstring(String s, int freq) {
+        /*int count = {};
         int start = 0, max_length = 0, max_repeat = 0;
         for(int i =0; i< s.length(); i++) {
             count[s.charAt(i)] =
-        }
+        }*/
     }
 
 }
